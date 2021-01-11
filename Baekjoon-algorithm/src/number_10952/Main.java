a6208862318780d78b2ca37ae231329619b55dad
82103 <82103@DESKTOP-6FVH2SO>
2021-01-11 23:09:32 +0900

    백준 알고리즘 10951, 10952, 15552번

---
+++ Baekjoon-algorithm/src/number_10952/Main.java
@@ -0,0 +1,34 @@
+package number_10952;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws  IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = "";
+		StringTokenizer st;
+			
+		while((str = br.readLine()) != null) {
+			st = new StringTokenizer(str," ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());			
+
+			bw.write(A+B + "\n");
+		}
+		br.close();
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
---
+++ Baekjoon-algorithm/src/number_15552/Main.java
@@ -0,0 +1,34 @@
+package number_15552;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws  IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int num = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+
+		for (int i = 0; i < num; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write(A+B + "\n");
+		}
+
+		br.close();
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
